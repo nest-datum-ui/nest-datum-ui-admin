6a4dc53f2063471366ebac95fde8fbefa60ccb6c
ihor <ihor.bielchenko@gmail.com>
2023-05-21 20:41:10 +0200

    Update logic.

--- src/@nest-datum-ui-admin-lib/john-connor/src/layouts/Layout.jsx
+++ src/@nest-datum-ui-admin-lib/john-connor/src/layouts/Layout.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
-// import ListNeuron from '../components/List/Neuron';
-// import ListInteract from '../components/List/Interact';
+import ListNeuron from '../components/List/Neuron';
+import ListInteract from '../components/List/Interact';
 
 let Layout = ({ children }) => {
-	return <Grid container>
+	return <Grid 
+		container
+		sx={{
+			height: 'calc(100% - 44px)',
+			width: 'calc(100% + 24px)',
+			marginLeft: '-24px',
+			borderTop: '1px solid #e0e0e0',
+		}}>
 		<Grid
 			item
-			xs={true}>
-			ListNeuron
+			xs={true}
+			sx={{
+				height: '100%',
+			}}>
+			<ListNeuron />
 		</Grid>
 		<Grid
 			item
-			xs={2}>
-			ListInteract
+			xs={2}
+			sx={{
+				height: '100%',
+			}}>
+			<ListInteract />
 		</Grid>
 	</Grid>;
 };
