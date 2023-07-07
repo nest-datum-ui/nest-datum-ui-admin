2569c6811ff20b5289a2680b84ff37c97e7842b8
Victor A <vartus2@gmail.com>
2023-07-07 14:59:40 +0300

    HAPP-518

--- src/@nest-datum-ui/Form/Search/Search.jsx
+++ src/@nest-datum-ui/Form/Search/Search.jsx
@@ -43,7 +43,7 @@ let Search = ({
 			actionApiListMerge(storeName, {
 				loader: true,
 				page: 1,
-				limit: 20,
+				limit: 0,
 				query,
 			})();
 			onSearch(String(e.target.elements[name].value ?? ''));
