2719bb680679031032f8465cf8a077b29abb55e1
Victor A <vartus2@gmail.com>
2023-07-26 18:47:30 +0300

    HAPP-331

--- src/@nest-datum-ui-admin-lib/sso/src/config/signIn.js
+++ src/@nest-datum-ui-admin-lib/sso/src/config/signIn.js
@@ -1,7 +1,7 @@
 
 const SignIn = {
 	pageTitle: 'Sign in',
-	pageFullUrl: `${process.env.URL_UI}/${process.env.ROUTE_SiGN_IN}`,
+	pageFullUrl: process.env.ROUTE_SiGN_IN,
 			
 	id: 'sso-sign-in',
 	storeName: 'sso-sign-in',
